3c4020f1124089e9307a74efb2d5cdff375de979
Preethi <51679039+preethical@users.noreply.github.com>
2021-05-10 17:40:42 -0600

    assam public procurement data initial analysis

--- Assam_tenders.R
+++ Assam_tenders.R
@@ -1,68 +1,129 @@
 library(tidyverse)
 library(lubridate)
-#readall the excel files
-Assamtender_201617_published <- readxl::read_excel("TenderDetailsExcel2016_17.xls", sheet=1, skip = 1,col_names = TRUE)
-Assamtender_201617_progress <- readxl::read_excel("TenderDetailsExcel2016_17.xls", sheet=2, skip = 1,col_names = TRUE)
-Assamtender_201617_noresp <- readxl::read_excel("TenderDetailsExcel2016_17.xls", sheet=3, skip = 1,col_names = TRUE)
-Assamtender_201617_aoc <- readxl::read_excel("TenderDetailsExcel2016_17.xls", sheet=4, skip = 1,col_names = TRUE)
+library(readxl)
+library(janitor)
+library(stringr)
 
-Assamtender_201718_published <- readxl::read_excel("TenderDetailsExcel2017_18.xls", sheet=1, skip = 1,col_names = TRUE)
-Assamtender_201718_progress <- readxl::read_excel("TenderDetailsExcel2017_18.xls", sheet=2, skip = 1,col_names = TRUE)
-Assamtender_201718_noresp <- readxl::read_excel("TenderDetailsExcel2017_18.xls", sheet=3, skip = 1,col_names = TRUE)
-Assamtender_201718_aoc <- readxl::read_excel("TenderDetailsExcel2017_18.xls", sheet=4, skip = 1,col_names = TRUE)
+#import, merge, remove duplicates and clean each sheet
+assam_files <- list.files(pattern = "*.xls")
 
-Assamtender_201819_published <- readxl::read_excel("TenderDetailsExcel2018_19.xls", sheet=1, skip = 1,col_names = TRUE)
-Assamtender_201819_progress <- readxl::read_excel("TenderDetailsExcel2018_19.xls", sheet=2, skip = 1,col_names = TRUE)
-Assamtender_201819_noresp <- readxl::read_excel("TenderDetailsExcel2018_19.xls", sheet=3, skip = 1,col_names = TRUE)
-Assamtender_201819_aoc <- readxl::read_excel("TenderDetailsExcel2018_19.xls", sheet=4, skip = 1,col_names = TRUE)
+#published
+assam_tenders_published <- lapply(assam_files, function(i){
+  assam_yearly = read_excel(path = i, sheet = 1, skip = 1, col_names = T)
+  assam_yearly$file = i
+  assam_yearly
+})
+assam_tenders_published<- do.call("bind_rows", assam_tenders_published)
+assam_tenders_published<- 
+  assam_tenders_published[!duplicated(assam_tenders_published[2]),]
 
-Assamtender_201920_published <- readxl::read_excel("TenderDetailsExcel2019_20.xls", sheet=1, skip = 1,col_names = TRUE)
-Assamtender_201920_progress <- readxl::read_excel("TenderDetailsExcel2019_20.xls", sheet=2, skip = 1,col_names = TRUE)
-Assamtender_201920_noresp <- readxl::read_excel("TenderDetailsExcel2019_20.xls", sheet=3, skip = 1,col_names = TRUE)
-Assamtender_201920_aoc <- readxl::read_excel("TenderDetailsExcel2019_20.xls", sheet=4, skip = 1,col_names = TRUE)
+assam_tenders_published <- clean_names(assam_tenders_published)
+assam_tenders_published$tender_title <- toupper(assam_tenders_published$tender_title)
+assam_tenders_published$organisation_chain <- toupper(assam_tenders_published$organisation_chain)
+assam_tenders_published$published_date <- as.Date(assam_tenders_published$published_date, 
+                                                  format="%d-%b-%Y")
+assam_tenders_published$pre_bid_meeting_date <- as.Date(assam_tenders_published$pre_bid_meeting_date, 
+                                                        format="%d-%b-%Y")
 
-Assamtender_202021_published <- readxl::read_excel("TenderDetailsExcel2020_21.xls", sheet=1, skip = 1,col_names = TRUE)
-Assamtender_202021_progress <- readxl::read_excel("TenderDetailsExcel2020_21.xls", sheet=2, skip = 1,col_names = TRUE)
-Assamtender_202021_noresp <- readxl::read_excel("TenderDetailsExcel2020_21.xls", sheet=3, skip = 1,col_names = TRUE)
-Assamtender_202021_aoc <- readxl::read_excel("TenderDetailsExcel2020_21.xls", sheet=4, skip = 1,col_names = TRUE)
+assam_tenders_published$bid_opening_date<- as.Date(assam_tenders_published$bid_opening_date, 
+                                                   format="%d-%b-%Y")
 
-Assamtender_published <- rbind(Assamtender_201617_published,Assamtender_201718_published,Assamtender_201819_published,Assamtender_201920_published,Assamtender_202021_published)
-Assamtender_published<- 
-  Assamtender_published[!duplicated(Assamtender_published[2]),]
+assam_tenders_published$price_bid_opening_date <- as.Date(assam_tenders_published$price_bid_opening_date, 
+                                                          format="%d-%b-%Y")
 
-Assamtender_progress <- rbind(Assamtender_201617_progress,Assamtender_201718_progress,Assamtender_201819_progress,Assamtender_201920_progress,Assamtender_202021_progress)
-Assamtender_progress<- 
-  Assamtender_progress[!duplicated(Assamtender_progress[2]),]
+assam_tenders_published$publishedyear <- year(assam_tenders_published$published_date)
 
-Assamtender_noresp <- rbind(Assamtender_201617_noresp,Assamtender_201718_noresp,Assamtender_201819_noresp,Assamtender_201920_noresp,Assamtender_202021_noresp)
-Assamtender_noresp<- 
-  Assamtender_noresp[!duplicated(Assamtender_noresp[2]),]
+assam_tenders_published <- assam_tenders_published %>% separate(organisation_chain, into = c("Org","role"), sep = "\\|\\|", extra = "merge")
 
-Assamtender_aoc <- rbind(Assamtender_201617_aoc,Assamtender_201718_aoc,Assamtender_201819_aoc,Assamtender_201920_aoc,Assamtender_202021_aoc)
-Assamtender_aoc<- 
-  Assamtender_aoc[!duplicated(Assamtender_aoc[2]),]
+assam_tenders_org <- assam_tenders_published %>% group_by(Org, publishedyear) %>% summarise(total = sum(value_of_tender_in_rs, na.rm = TRUE)) %>% top_n(n = 5, wt = total)
 
-Assamtendermerged <- merge(Assamtender_published, Assamtender_aoc,all = TRUE)
-tendermerge1 <- merge(Assamtendermerged, Assamtender_progress, all = TRUE)
-Finalmerge <- merge(tendermerge1, Assamtender_noresp, all = TRUE)
-Finalmerge<- 
-  Finalmerge[!duplicated(Finalmerge[2]),]
+tenders <- assam_tenders_org %>% top_n(5) %>% ggplot()+ aes(x = publishedyear, y=total)+ geom_bar(stat = "identity", position = "dodge",width = 0.7)
 
-Finalmerge$`Published Date` <- as.Date(Finalmerge$`Published Date`, 
-                                      format="%d-%b-%Y")
-Finalmerge$`PreBid Meeting Date` <- as.Date(Finalmerge$`PreBid Meeting Date`, 
-                                    format="%d-%b-%Y")
+ggplot(assam_tenders_org, aes(x = Org, y = total)) +
+         geom_col() +
+         facet_grid(.~publishedyear, scales = "free")
 
-Finalmerge$`Price Bid Opening Date` <- as.Date(Finalmerge$`Price Bid Opening Date`, 
-                                          format="%d-%b-%Y")
+#in Progress
+assam_tenders_progress <- lapply(assam_files, function(i){
+  assam_yearly = read_excel(path = i, sheet = 2, skip = 1, col_names = T)
+  assam_yearly$file = i
+  assam_yearly
+})
+assam_tenders_progress<- do.call("bind_rows", assam_tenders_progress)
 
-Finalmerge$`Stage/Status updated on` <- as.Date(Finalmerge$`Stage/Status updated on`, 
-                                            format="%d-%b-%Y")
+assam_tenders_progress<- 
+  assam_tenders_progress[!duplicated(assam_tenders_progress[2]),]
 
-Finalmerge$`Date of Award Of Contract` <- as.Date(Finalmerge$`Date of Award Of Contract`, 
-                                             format="%d-%b-%Y")
+assam_tenders_progress <- clean_names(assam_tenders_progress)
+assam_tenders_progress$tender_title <- toupper(assam_tenders_progress$tender_title)
+assam_tenders_progress$organisation_chain <- toupper(assam_tenders_progress$organisation_chain)
+assam_tenders_progress$published_date <- as.Date(assam_tenders_progress$published_date, 
+                                                  format="%d-%b-%Y")
+assam_tenders_progress$pre_bid_meeting_date <- as.Date(assam_tenders_progress$pre_bid_meeting_date, 
+                                                        format="%d-%b-%Y")
 
-Finalmerge$publishedyear <- year(Finalmerge$`Published Date`)
+assam_tenders_progress$bid_opening_date<- as.Date(assam_tenders_progress$bid_opening_date, 
+                                                   format="%d-%b-%Y")
 
-org_plot<- ggplot(Finalmerge, aes(x=Finalmerge$`Organisation Chain`, y= tally()))+ 
-  geom_boxplot() + theme(axis.text.x = element_text(size = 6, angle = 90))
+assam_tenders_progress$price_bid_opening_date <- as.Date(assam_tenders_progress$price_bid_opening_date, 
+                                                          format="%d-%b-%Y")
+
+assam_tenders_progress$publishedyear <- year(assam_tenders_progress$published_date)
+
+
+#noresponse
+assam_tenders_noresp <- lapply(assam_files, function(i){
+  assam_yearly = read_excel(path = i, sheet = 3, skip = 1, col_names = T)
+  assam_yearly$file = i
+  assam_yearly
+})
+assam_tenders_noresp<- do.call("bind_rows", assam_tenders_noresp)
+assam_tenders_noresp<- 
+  assam_tenders_noresp[!duplicated(assam_tenders_noresp[2]),]
+assam_tenders_noresp <- clean_names(assam_tenders_noresp)
+
+assam_tenders_noresp$tender_title <- toupper(assam_tenders_noresp$tender_title)
+assam_tenders_noresp$organisation_chain <- toupper(assam_tenders_noresp$organisation_chain)
+assam_tenders_noresp$published_date <- as.Date(assam_tenders_noresp$published_date, 
+                                                 format="%d-%b-%Y")
+assam_tenders_noresp$pre_bid_meeting_date <- as.Date(assam_tenders_noresp$pre_bid_meeting_date, 
+                                                       format="%d-%b-%Y")
+
+assam_tenders_noresp$bid_opening_date<- as.Date(assam_tenders_noresp$bid_opening_date, 
+                                                  format="%d-%b-%Y")
+
+assam_tenders_noresp$publishedyear <- year(assam_tenders_noresp$published_date)
+
+#aoc
+assam_tenders_aoc <- lapply(assam_files, function(i){
+  assam_yearly = read_excel(path = i, sheet = 4, skip = 1, col_names = T)
+  assam_yearly$file = i
+  assam_yearly
+})
+assam_tenders_aoc<- do.call("bind_rows", assam_tenders_aoc)
+assam_tenders_aoc<- 
+  assam_tenders_aoc[!duplicated(assam_tenders_aoc[2]),]
+
+assam_tenders_aoc <- clean_names(assam_tenders_aoc)
+
+assam_tenders_aoc$tender_title <- toupper(assam_tenders_aoc$tender_title)
+assam_tenders_aoc$organisation_chain <- toupper(assam_tenders_aoc$organisation_chain)
+assam_tenders_aoc$published_date <- as.Date(assam_tenders_aoc$published_date, 
+                                               format="%d-%b-%Y")
+assam_tenders_aoc$pre_bid_meeting_date <- as.Date(assam_tenders_aoc$pre_bid_meeting_date, 
+                                                     format="%d-%b-%Y")
+
+assam_tenders_aoc$bid_opening_date<- as.Date(assam_tenders_aoc$bid_opening_date, 
+                                                format="%d-%b-%Y")
+assam_tenders_aoc$date_of_award_of_contract <- as.Date(assam_tenders_aoc$date_of_award_of_contract,
+                                                       format="%d-%b-%Y" )
+
+assam_tenders_aoc$publishedyear <- year(assam_tenders_aoc$published_date)
+
+#merge all these sheets to produce a master one
+
+assam_tender_merged <- do.call (merge, list(assam_tenders_published, assam_tenders_progress,assam_tenders_noresp, assam_tenders_aoc))
+assam_tender_merged<- 
+  assam_tender_merged[!duplicated(assam_tender_merged[2]),] 
+  
+
