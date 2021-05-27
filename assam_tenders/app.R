f5f4a3eab3d65be63a88ab8553c3606987db2a70
Preethi <51679039+preethical@users.noreply.github.com>
2021-05-27 15:05:47 -0600

    added two new plots to this shiny app.

--- assam_tenders/app.R
+++ assam_tenders/app.R
@@ -5,36 +5,61 @@ library(plotly)
 library(psych)
 library(lubridate)
 assam_published <- read.csv("assam_tenders_published.csv", header = T)
-print(str(glimpse(assam_published)))
+
 ui <- fluidPage(titlePanel("Basic Exploratory Dashboard"),
                 sidebarLayout(
                     sidebarPanel(
                         helpText("The various inputs this takes are Tender Organization, product category"),
-                        selectizeInput("Org", label="Organization", choices =  unique(assam_published$Org), options = list(maxItems = 4, placeholder = "Please select an Organization")),
-                        selectizeInput("product_category", label="Product Category", choices =  unique(assam_published$product_category), options = list(placeholder = "Please select a type of product")),
+                        uiOutput("Org_output"),
+                        uiOutput("category_output"),
                         radioButtons("KPI", label = "Key Performance Indicator", 
                                      choices = c("Tender Stage","Tender Status","Number of bids received","Number of days for AOC", "Number of days for evaluation"),
-                                     selected = "Tender Stage")
+                                     selected = "Tender Stage"), 
+                        actionButton(inputId="DatabyPage", label="Calculate")
                         ),
                                     mainPanel(
-                                        plotOutput("Tender_stage_plot"),
-                                        plotOutput("Tender_status_plot"),
-                                        plotOutput("bids_recieved"),
+                                        plotlyOutput("tender_stage_plot"),
+                                        plotlyOutput("bids_recieved"),
                                         plotlyOutput("AOC_days"),
-                                        plotOutput("Evaluation_days"),
-                                        br(), br(),
+                                        plotlyOutput("Evaluation_days"),
                                         tableOutput("results")
                                     )
                                )
                 )
             
-server <- function(input, output) {
-    output$Tender_stage_plot <- renderPlot({
-        filtered <- assam_published %>% filter(Org == input$Org, type == input$product_category)
-      ggplot(filtered, aes(x=Org, y = tender_stage)) + 
-            geom_boxplot(outlier.colour="red", outlier.shape=8,outlier.size=4)
-    }
-    )
+server <- function(input, output, session) {
+    output$Org_output <- renderUI ({
+        selectizeInput("input_Org", label="Organization", choices =  sort(unique(assam_published$Org)), 
+                       multiple = TRUE, selected = NULL)
+    })
+    output$category_output <- renderUI ({
+        selectizeInput("input_category", label="Tender Category",
+                       choices =  sort(unique(assam_published$tender_category)), multiple = TRUE, selected = NULL)
+    })    
+        filtered <- reactive({
+            if (is.null(input$input_Org)) {
+                return(NULL)
+            } 
+        assam_published %>% filter(Org %in% input$input_Org, tender_category %in% input$input_category)
+        })
+    output$tender_stage_plot <- renderPlotly({
+        if (is.null(filtered())) {
+            return()
+        }
+        ggplotly(ggplot(data = filtered(), aes(x = tender_stage,fill = tender_status)) + 
+                     geom_bar(position = "dodge") + facet_grid(~publishedyear, scales = "free") + 
+                     theme(axis.text.x=element_text(angle=90,hjust=1,vjust=.5,colour='black')))
+    })
+    output$bids_recieved<- renderPlotly({
+        if (is.null(filtered())) {
+            return()
+        }
+        ggplotly(ggplot(data = filtered(), aes(x = publishedyear, y = no_of_bids_received)) + 
+                     geom_boxplot(outlier.colour="red", outlier.shape=8,
+                                  outlier.size=4))
+    })
+    output$results <- renderTable({
+        filtered()
+    })
 }
-
 shinyApp(ui = ui, server = server)
